22f69a2e6f5d64437811bd82545eefdc8c5c9688
Sawyer X <xsawyerx@cpan.org>
2010-09-09 00:00:31 +0300

    moving to object structure

--- lib/App/cpang.pm
+++ lib/App/cpang.pm
@@ -10,14 +10,23 @@ our $VERSION = '0.03';
 
 sub new {
     my $class = shift;
-    my $self  = bless {}, $class;
+    my $self  = bless {
+        main_window => Gtk2::Window->new,
+        terminal    => Gnome2::Vte::Terminal->new,
+        scrollbar   => Gtk2::VScrollbar->new,
+        status      => Gtk2::Statusbar->new,
+    }, $class;
     return $self;
 }
 
 sub run {
-    my $self = shift;
+    my $self      = shift;
+    my $terminal  = $self->{'terminal'};
+    my $scrollbar = $self->{'scrollbar'};
+    my $status    = $self->{'status'};
+    my $window    = $self->{'main_window'};
+
     # create a nice window
-    my $window = Gtk2::Window->new;
     $window->set_title('cpang');
     $window->signal_connect( destroy => sub { Gtk2->main_quit; } );
     $window->set_border_width(5);
@@ -36,63 +45,70 @@ sub run {
 
     # create an entry (textbox) and put it in the hbox
     my $entry = Gtk2::Entry->new;
-    $entry->signal_connect( 'activate' => \&click );
+    $entry->signal_connect(
+        'activate' => sub { $self->click( $entry ) }
+    );
 
     $hbox->pack_start( $entry, TRUE, TRUE, 0 );
 
     # create a button and put it in the hbox
     my $button = Gtk2::Button->new('Install');
-    $button->signal_connect( clicked => \&click );
+    $button->signal_connect(
+        clicked => sub { $self->click( $entry ) }
+    );
     $hbox->pack_start( $button, FALSE, TRUE, 0 );
 
     # create a terminal and put it in the vbox too
-    my $terminal  = Gnome2::Vte::Terminal->new();
-    my $scrollbar = Gtk2::VScrollbar->new;
     $scrollbar->set_adjustment( $terminal->get_adjustment );
     $vbox->pack_start( $terminal, TRUE, TRUE, 0 );
-    $terminal->signal_connect( child_exited => sub { $entry->set_editable(1); } );
+    $terminal->signal_connect(
+        child_exited => sub { $entry->set_editable(1) }
+    );
 
-    my $status = Gtk2::Statusbar->new ();
     $vbox->pack_end ($status, FALSE, FALSE, 0);
     $window->show_all;
     $terminal->hide();
     Gtk2->main;
+}
 
-    sub click {
-        my ($object) = @_;
-        my $text = $entry->get_text() || q{};
-        if ($text) {
-            $entry->set_editable(0);
-            $entry->set_text('');
+sub click {
+    my ( $self, $entry ) = @_;
+    my $status   = $self->{'status'};
+    my $terminal = $self->{'terminal'};
+    my $window   = $self->{'main_window'};
+    my $text     = $entry->get_text() || q{};
 
-            $terminal->show();
-            $status->pop (0);
-            $status->push (0, "Installing $text...");
+    if ($text) {
+        $entry->set_editable(0);
+        $entry->set_text('');
 
+        $terminal->show();
+        $status->pop (0);
+        $status->push (0, "Installing $text...");
+
+        my $cmd_result = $terminal->fork_command(
+            'cpanm', [ 'cpanm', $text ],
+            undef, '/tmp', FALSE, FALSE, FALSE,
+        );
+
+        if ( $cmd_result == -1 ) {
             my $cmd_result = $terminal->fork_command(
-                'cpanm', [ 'cpanm', $text ],
+                'sudo', [ 'sudo', 'cpan', '-i', $text ],
                 undef, '/tmp', FALSE, FALSE, FALSE,
             );
 
             if ( $cmd_result == -1 ) {
-                my $cmd_result = $terminal->fork_command(
-                    'sudo', [ 'sudo', 'cpan', '-i', $text ],
-                    undef, '/tmp', FALSE, FALSE, FALSE,
+                print STDERR "Cannot find 'cpanm' command\n";
+                my $dialog = Gtk2::MessageDialog->new(
+                    $window,
+                    'destroy-with-parent',
+                    'warning',
+                    'ok',
+                    'Cannot find "sudo", "cpan" or "cpanm" program',
                 );
 
-                if ( $cmd_result == -1 ) {
-                    print STDERR "Cannot find 'cpanm' command\n";
-                    my $dialog = Gtk2::MessageDialog->new(
-                        $window,
-                        'destroy-with-parent',
-                        'warning',
-                        'ok',
-                        'Cannot find "sudo", "cpan" or "cpanm" program',
-                    );
-
-                    $dialog->run;
-                    $dialog->destroy;
-                }
+                $dialog->run;
+                $dialog->destroy;
             }
         }
     }
