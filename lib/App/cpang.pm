6e5a86a106b4e5ae3d9d7636ebaf4a72e787a40c
Sawyer X <xsawyerx@cpan.org>
2011-01-08 14:57:14 +0200

    adding still-not-working run_search method

--- lib/App/cpang.pm
+++ lib/App/cpang.pm
@@ -53,6 +53,75 @@ sub gtk_main_quit {
     Gtk2->main_quit;
 }
 
+sub run_search {
+    my $self      = shift;
+    my $widget    = shift;
+    my $searchbox = $gui->get_widget('searchtextbox');
+
+    # decide what kind of search we want
+    my $widget_name = $widget->get_name();
+    if ( $widget_name eq 'distbutton' ) {
+        # we want a dist search
+    } elsif ( $widget_name eq 'authorbutton' ) {
+        # we want an author search
+    } else {
+        # we want a dist search
+        # TODO: default
+    }
+
+    my $tree_widget = $gui->get_widget('searchresults');
+    my $resultslist = Gtk2::Ex::Simple::List->new_from_treeview(
+        $tree_widget =>
+        ''            => 'bool',
+        'Dist name'   => 'text',
+        'Latest'      => 'text',
+        'Installed'   => 'text',
+        'Author'      => 'text',
+        'Description' => 'text',
+    );
+#    $tree_widget->{data} is an ARRAYREF
+
+    my $searchterm = $searchbox->get_text or return;
+
+    #$resultslist->set_headers_clickable(1);
+    foreach my $col ( $resultslist->get_columns() ) {
+        $col->set_resizable(1);
+        $col->set_sizing('grow-only');
+    }
+
+    my $sth = $dbh->prepare('SELECT DISTINCT distribution, module FROM module WHERE module LIKE ? ORDER BY distribution');
+    $sth->execute( "%$searchterm%" );
+    my $arrayref = $sth->fetchall_arrayref;
+
+    foreach my $distref ( @{$arrayref} ) {
+        my $distname   = shift @{$distref};
+        my $modulename = shift @{$distref};
+
+        # get dist information
+        $sth = $dbh->prepare('SELECT version, author FROM distribution WHERE distribution = ?');
+        $sth->execute($distname);
+        my $distdata = $sth->fetchrow_arrayref;
+
+        my ( $latest_version, $author_id ) = $distdata    ?
+                                             @{$distdata} :
+                                             ( 'no info', 'no info' );
+
+        my $installed_version = get_version($modulename);
+
+        push @{ $resultslist->{'data'} },
+            [
+                FALSE,
+                $modulename,
+                $latest_version,
+                $installed_version,
+                $author_id,
+                'A postmodern object system for Perl 5',
+            ];
+    }
+
+    $resultslist->select(0);
+}
+
 1;
 
 __END__
